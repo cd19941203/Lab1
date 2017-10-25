6afdec967878e8b95304f669ecc8e42a173e557a
Test <eden4011@yahoo.com.tw>
2017-10-25 12:05:00 +0800

    Eden's update

--- lab.py
+++ lab.py
@@ -5,3 +5,7 @@ print('妳在哪裡')
 print('Waar ben je? (荷蘭文)')
 print('Non zaude? (巴斯克文)')
 print('Ki kote ou ye? (海地克里奧文)')
+
+print('어디 있니? (韓文)')
+print('Hvor er du? (丹麥文)')
+print('คุณอยู่ที่ไหน? ()泰文')
